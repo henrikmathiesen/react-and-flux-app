0ea29775310158bd1844c213a2d8dde083bf0fda
Henrik <henrik_mathiesen@hotmail.com>
2016-07-17 20:34:51 +0200

    -set up

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var argv = require('yargs').argv;
+
+var isProduction = (argv.prod) ? (true) : (false); // gulp --prod
 
 var config = {
     port: 1337,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './app/*.html',
+        mainJS: './app/main.js',
+        js: './app/**/*.js',
         bld: './bld'
     }
 };
@@ -18,13 +26,23 @@ gulp.task('connect', function(){
     })
 });
 
+gulp.task('js', function(){
+    browserify(config.paths.mainJS, { debug: !isProduction })
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.bld));
+});
+
 gulp.task('html', function(){
-    gulp.src(config.paths.html)
+    return gulp.src(config.paths.html)
         .pipe(gulp.dest(config.paths.bld))
 });
 
 gulp.task('watch', function(){
     gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
 });
 
-gulp.task('default', ['html', 'connect', 'watch']);
+gulp.task('default', ['html', 'js', 'connect', 'watch']);

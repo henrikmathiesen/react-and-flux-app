47887b79f3236f6a53d8ed728e7bb00098ae4bc3
Henrik <henrik_mathiesen@hotmail.com>
2016-07-18 02:16:56 +0200

    -homePage component

---
+++ app/js/components/homePage.js
@@ -0,0 +1,18 @@
+var React = require('react');
+
+var Home = React.createClass({
+    render: function(){
+        /* jshint ignore:start */
+        return(
+            <div className="jumbotron">
+                <h1>Pluralsight Administration</h1>
+                <p>
+                    We are using React, React Router and Flux for ultra responsive web apps.
+                </p>
+            <div>
+        )
+        /* jshint ignore:end */
+    }
+});
+
+module.exports = Home;

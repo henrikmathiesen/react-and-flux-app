3ca4a28f5220b8e7e86394f3f203adc1a87d1350
Henrik <henrik_mathiesen@hotmail.com>
2016-07-19 03:44:34 +0200

    -router links

--- app/js/components/Header.js
+++ app/js/components/Header.js
@@ -4,22 +4,27 @@ var Link = ReactRouter.Link;
 var IndexLink = ReactRouter.IndexLink;
 
 var Header = React.createClass({
-    render: function () {
 
+    getIsActiveClass: function(route){
         var history = this.props.history;
-        
-        var getIsActiveClass = function(route){
-            return history.isActive(route) ? 'active' : '';
-        };
+        return history.isActive(route) ? 'active' : '';
+    },
 
+    getIndexIsActiveClass: function(){
+        var hash = window.location.hash.slice(1);   // after #
+        var path = hash.split('?')[0];              // before ?
+        return path === '/' ? 'active' : '';
+    },
+
+    render: function () {
         return(
             <nav className="navbar navbar-default">
                 <div className="container-fluid">
                     <ul className="nav navbar-nav">
-                        <li className={getIsActiveClass('/')}><IndexLink to="/">Home</IndexLink></li>
-                        <li className={getIsActiveClass('authors')}><Link to="authors">Authors</Link></li>
-                        <li className={getIsActiveClass('about')}><Link to="about">About</Link></li>
-                        <li className={getIsActiveClass('info')}><Link to="info">Info</Link></li>
+                        <li className={this.getIndexIsActiveClass()}><IndexLink to="/">Home</IndexLink></li>
+                        <li className={this.getIsActiveClass('authors')}><Link to="authors">Authors</Link></li>
+                        <li className={this.getIsActiveClass('about')}><Link to="about">About</Link></li>
+                        <li className={this.getIsActiveClass('info')}><Link to="info">Info</Link></li>
                     </ul>
                 </div>
             </nav>
